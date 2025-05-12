c9ab808f32c407d7604b3cabc499d37790574e73
yzahir <youssef50zouhayr@gmail.com>
2025-05-12 17:03:11 +0200

    Add files via upload

---
+++ client.py
@@ -0,0 +1,45 @@
+import paho.mqtt.client as mqtt
+import json
+import time
+from pipuck.pipuck import PiPuck
+
+# Define variables and callbacks
+Broker = "192.168.178.56"  # Replace with your broker address
+Port = 1883 # standard MQTT port
+
+# function to handle connection
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    client.subscribe("robot_pos/all")
+
+# function to handle incoming messages
+def on_message(client, userdata, msg):
+    try:
+        data = json.loads(msg.payload.decode())
+        print(data)
+    except json.JSONDecodeError:
+        print(f'invalid json: {msg.payload}')
+
+# Initialize MQTT client
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(Broker, Port, 60)
+
+client.loop_start() # Start listening loop in separate thread
+
+# Initialize the PiPuck
+pipuck = PiPuck(epuck_version=2)
+
+# Set the robot's speed, e.g. with
+pipuck.epuck.set_motor_speeds(1000,-1000)
+
+for _ in range(1000):
+    # TODO: Do your stuff here
+	time.sleep(1)
+	
+    
+# Stop the MQTT client loop
+pipuck.epuck.set_motor_speeds(0,0)
+client.loop_stop()  
