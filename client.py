b66c84222bd6f67a4456ca0824990950104f3133
yzahir <youssef50zouhayr@gmail.com>
2025-05-13 11:11:49 +0200

    Update client.py

--- client.py
+++ client.py
@@ -8,7 +8,6 @@ import random
 # Define variables and callbacks
 Broker = "192.168.178.56"  # Replace with your broker address
 Port = 1883 # standard MQTT port
-data
 # function to handle connection
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
