38fb66576ccee71888454e4a751e959304fd610b
yzahir <youssef50zouhayr@gmail.com>
2025-05-20 12:03:28 +0200

    Update client.py

--- client.py
+++ client.py
@@ -3,10 +3,12 @@ import json
 import time
 from pipuck.pipuck import PiPuck
 import random
+import math
 
 pos = {}
 all_pos = {}
-robot_id = "35" 
+robot_id = "40" 
+runner_id = "2"
 
 # Define variables and callbacks
 Broker = "192.168.178.56"  # Replace with your broker address
@@ -59,47 +61,52 @@ client.loop_start() # Start listening loop in separate thread
 #pipuck.set_led_rgb(0, 1, 0, 0)
 workspace_Lboarder = 50
 workspace_RBoarder = 60
+TURN_SPEED = 200
+Forward_SPEED = 500
 try:
     last_pub_time = time.time()
     publish_interval = 3 # 3 seconds
     for _ in range(1000):
-        
-        speed = random.randint(300,1000)
-        turn_speed = random.randint(100, 300)
-        duration = random.randint(1,10)
-        direction = random.choice(["left", "right"])
-        start_time = time.time()
-        #pipuck.epuck.set_motor_speeds(speed,speed)
-        if "position" in pos:
-            current_pos = pos["position"]
-            avoid_turn = 500
-            if(current_pos[0]<0.3 or  current_pos[0]>1.8 or current_pos[1]>0.8 or current_pos[1]<0.3):
-                pipuck.epuck.set_motor_speeds(-avoid_turn, avoid_turn)
-                time.sleep(0.3)
-                pipuck.epuck.set_motor_speeds(speed,speed)
-                time.sleep(0.8)
-                continue
-        pipuck.epuck.set_motor_speeds(speed,speed)
-        time.sleep(duration/10.0)
-        if direction == "left":
-            pipuck.epuck.set_motor_speeds(-turn_speed, turn_speed)
-        else:
-            pipuck.epuck.set_motor_speeds(turn_speed, -turn_speed)
-        time.sleep(duration/10.0)
+        for rbt_id, robot_data in all_pos.items():
+            robot_angle = robot_data.get("angle", 0)
+            if rbt_id == runner_id:
+                x, y = pos["position"]
+                runner_x, runner_y = robot_data["position"]
+                my_angle = pos["angle"]
+                dx = runner_x-x
+                dy = runner_y-y
+                d_angle = math.degrees(math.atan2(dy, dx))
+                angle_diff = abs(robot_angle - my_angle)
+                if angle_diff < 5:
+                    if angle_diff > 0:
+                        pipuck.epuck.set_motor_speeds(TURN_SPEED, TURN_SPEED)
+                    else:
+                        pipuck.epuck.set_motor_speeds(TURN_SPEED, -TURN_SPEED)
+                    time.sleep(TURN_SPEED/angle_diff)
+                    pipuck.epuck.set_motor_speeds(0, 0)
 
-        if time.time() - last_pub_time> publish_interval:
-            for rbt_id, robot_pos in all_pos.items():
-                if rbt_id == robot_id:
-                    x = robot_pos["position"][0]
-                    y = robot_pos["position"][1]
-                if (robot_pos["position"][0]-current_pos[0])**2 + (robot_pos["position"][1]-current_pos[1])**2 < 0.5:
-                    print(f"robot {rbt_id} is 50 cm away")
-                    str = f"robot_pos/{rbt_id}"
-                    client.publish(str, "Hello")
-                    last_pub_time = time.time()
-                    continue
+                else:
+                    if "position" in pos:
+                        current_pos = pos["position"]
+                        if(current_pos[0]<0.3 or  current_pos[0]>1.8 or current_pos[1]>0.8 or current_pos[1]<0.3):
+                            pipuck.epuck.set_motor_speeds(-TURN_SPEED, TURN_SPEED)
+                            time.sleep(0.3)
+                            pipuck.epuck.set_motor_speeds(Forward_SPEED,Forward_SPEED)
+                            time.sleep(0.8)
+                            continue
+                    pipuck.epuck.set_motor_speeds(Forward_SPEED,Forward_SPEED)
+                    time.sleep(math.sqrt(dx**2 + dy**2)/Forward_SPEED)
+
+                    if (runner_x-x)**2 + (runner_y-y)**2 < 0.2:
+                        print(f"robot {rbt_id} is 20 cm away")
+                        str = f"robot_pos/{rbt_id}"
+                        client.publish(str, "caught")
+                        pipuck.set_leds_colour("magenta")
+                        time.sleep(0.1)
+                        pipuck.set_leds_colour("off")
+
+                        #continue
                                     
-
 except KeyboardInterrupt:
     print("Interrupt detected!!")
 finally:
