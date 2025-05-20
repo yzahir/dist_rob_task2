c8b0246fa20b112076cb46b31a9cf01f84e46b59
yzahir <youssef50zouhayr@gmail.com>
2025-05-20 10:18:01 +0200

    Update client.py

--- client.py
+++ client.py
@@ -6,16 +6,20 @@ import random
 
 pos = {}
 all_pos = {}
-robot_id = "35" 
+robot_id = "40" 
 
 # Define variables and callbacks
 Broker = "192.168.178.56"  # Replace with your broker address
 Port = 1883 # standard MQTT port
+
+# Initialize the PiPuck
+pipuck = PiPuck(epuck_version=2)
+
 # function to handle connection
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     client.subscribe("robot_pos/all")
-    client.subscribe(f"robot/+")
+    client.subscribe("robot/+")
 
 # function to handle incoming messages
 def on_message(client, userdata, msg):
@@ -29,6 +33,7 @@ def on_message(client, userdata, msg):
             pipuck.set_leds_colour("magenta")
             time.sleep(0.1)
             pipuck.set_leds_colour("off")
+            return
         all_pos = data
         if robot_id in data:
             pos = data[robot_id]
@@ -46,7 +51,7 @@ client.connect(Broker, Port, 60)
 client.loop_start() # Start listening loop in separate thread
 
 # Initialize the PiPuck
-pipuck = PiPuck(epuck_version=2)
+#pipuck = PiPuck(epuck_version=2)
 
 # Set the robot's speed, e.g. with
 #pipuck.epuck.set_motor_speeds(1000,-1000)
@@ -95,7 +100,7 @@ try:
                     str = f"robot_pos/{rbt_id}"
                     client.publish(str, "Hello")
                     last_pub_time = time.time()
-                    continue
+                    #continue
                                     
 
 except KeyboardInterrupt:
